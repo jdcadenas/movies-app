19bcbc28c8a0a5b686a54611d2dfa3bcfa5d8cd9
José Cadenas <jdcadenas@gmail.com>
2024-04-05 16:34:59 -0400

    agregacion de interfaces

--- src/movies/components/Carrusel.tsx
+++ src/movies/components/Carrusel.tsx
@@ -11,34 +11,32 @@ import "swiper/css/navigation";
 import { Box, Typography } from "@mui/material";
 import { MovieResponse, MovieShort } from "../interfaces";
 
-interface Props{
-  titulo: string,
-  slug: string
+interface Props {
+  titulo: string;
+  slug: string;
 }
 
-
-export const Carrusel: FC<Props> = ({titulo,slug}) => {
+export const Carrusel: FC<Props> = ({ titulo, slug }) => {
   const [peliculas, setpeliculas] = useState<MovieShort[]>([]); //array de películas
   const obtenerPeliculas = async () => {
-    
-    const url =
-      `${import.meta.env.VITE_API_URL}/${slug}api_key=${import.meta.env.VITE_API_KEY}`;
+    const url = `${import.meta.env.VITE_API_URL}/${slug}api_key=${
+      import.meta.env.VITE_API_KEY
+    }`;
     const res = await fetch(url);
     const data: MovieResponse = await res.json();
     setpeliculas(data.results);
   };
 
   useEffect(() => {
-
     obtenerPeliculas();
   }, []);
   return (
-    <Box sx={{ marginbottom:6}}>
+    <Box sx={{ marginbottom: 6 }}>
       <Typography
         fontWeight={600}
         sx={{ color: "#fff", fontsize: 20, marginBottom: 1 }}
       >
-        { titulo }
+        {titulo}
       </Typography>
       <Swiper
         modules={[Navigation]}
